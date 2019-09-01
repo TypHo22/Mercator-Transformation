497d783054b89900b0aa99c29462e762183b292c
TypHo22 <42981587+TypHo22@users.noreply.github.com>
2019-09-01 21:06:39 +0200

    Update MercatorTransformation.h

--- MercatorTransformation.h
+++ MercatorTransformation.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <math.h>
 
 /*
@@ -13,10 +15,9 @@ in Indonesia prior to the introduction of the Universal Transverse Mercator, a s
 the projection. This has the same effect as choosing two standard parallels on which the true scale is
 maintained at equal north and south latitudes either side of the equator.
 Further Information in "Map Projections - A working Manual" can be find on www.epsg.org.
-
 Andreas Bernatzky 25.08.2019
  */
- 
+
 #define D_R (M_PI / 180.0)
 #define R_D (180.0 / M_PI)
 #define EarthRadiusX 6378137.0
@@ -25,41 +26,14 @@ Andreas Bernatzky 25.08.2019
 #define ECCENT (sqrt(1.0 - (RATIO * RATIO)))
 #define COM (0.5 * ECCENT)
 
-static double deg_rad (double ang) {
-        return ang * D_R;
-}
+static double deg_rad(double ang);
 //Converts longitude coordinates into x-Coordinate of Mercator Transformation
-double GeoToMerc_X (double lon) {
-        return EarthRadiusX * deg_rad (lon);
-}
+double GeoToMerc_X(double lon);
 //Converts latitude coordinates into y-Coordinate of Mercator Transformation
-double GeoToMerc_Y (double lat) {
-        lat = fmin (89.5, fmax (lat, -89.5));
-        double phi = deg_rad(lat);
-        double sinphi = sin(phi);
-        double con = ECCENT * sinphi;
-        con = pow((1.0 - con) / (1.0 + con), COM);
-        double ts = tan(0.5 * (M_PI * 0.5 - phi)) / con;
-        return 0 - EarthRadiusX * log(ts);
-}
+double GeoToMerc_Y(double lat);
 
-static double rad_deg (double ang) {
-        return ang * R_D;
-}
+static double rad_deg(double ang);
 //Converts Mercator x-Coordinates back into Geocoordinates Longitude
-double MercToGeo_Lon (double x) {
-        return rad_deg(x) / EarthRadiusX;
-}
+double MercToGeo_Lon(double x);
 //Converts Mercator y-Coordinates back into Geocoordinates Latitude
-double MercToGeo_Lat (double y) {
-        double ts = exp ( -y / EarthRadiusX);
-        double phi = M_PI_2 - 2 * atan(ts);
-        double dphi = 1.0;
-        int i;
-        for (i = 0; fabs(dphi) > 0.000000001 && i < 15; i++) {
-                double con = ECCENT * sin (phi);
-                dphi = M_PI_2 - 2 * atan (ts * pow((1.0 - con) / (1.0 + con), COM)) - phi;
-                phi += dphi;
-        }
-        return rad_deg (phi);
-}
+double MercToGeo_Lat(double y);
